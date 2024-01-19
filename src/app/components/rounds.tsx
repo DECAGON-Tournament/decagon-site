ab4cdd41280f3a6beb23ea483db0058afbee57e4
Ramy Kaddouri <106203063+rk234@users.noreply.github.com>
2024-01-19 18:09:56 -0500

    style edits

--- src/app/components/rounds.tsx
+++ src/app/components/rounds.tsx
@@ -5,10 +5,10 @@ const jbm = JetBrains_Mono({subsets: ["latin"], style: ["normal"]})
 export default function Rounds() {
     return <main className='flex flex-col xl:flex-row px-8 gap-5 justify-between bg-slate-950'>
         <div className='bg-slate-900 p-4 rounded-md border-2 border-sky-900 xl:max-w-80'>
-            <h1 className={`${jbm.className} text-lg text-sky-300 mb-4 `}>Individual Round</h1>
-            <div>
+            <h1 className={`${jbm.className} text-lg text-sky-300 mb-4 font-bold`}>Individual Round</h1>
+            <div className='text-slate-200'>
                 <p>Competitors will pick 2 of the following categories to compete in:</p>
-                <ul className='ml-4 list-disc'>
+                <ul className='ml-4 list-disc my-2'>
                     <li>Algebra/Number Theory</li>
                     <li>Geometry</li>
                     <li>Probability/Combinatorics</li>
@@ -17,8 +17,8 @@ export default function Rounds() {
             </div>
         </div>
         <div className='bg-slate-900 p-4 rounded-md border-2 border-sky-900 xl:max-w-80'>
-            <h1 className={`${jbm.className} text-lg text-sky-300 mb-4`}>Team Round</h1>
-            <div>
+            <h1 className={`${jbm.className} text-lg text-sky-300 mb-4 font-bold`}>Team Round</h1>
+            <div className='text-slate-200'>
                 <p>
                 Teams of up to 4 will take the team test.
                 Competitors may form prearranged teams.
@@ -29,8 +29,8 @@ export default function Rounds() {
             </div>
         </div>
         <div className='bg-slate-900 p-4 rounded-md border-2 border-sky-900 xl:max-w-80'>
-            <h1 className={`${jbm.className} text-lg text-sky-300 mb-4`}>Countdown Round</h1>
-            <div>
+            <h1 className={`${jbm.className} text-lg text-sky-300 mb-4 font-bold`}>Countdown Round</h1>
+            <div className='text-slate-200'>
                 <p>
                 The top 4 competitors from each individual category will participate in a countdown round for that category. Competitors will compete head-to-head in this round. A problem will be displayed through a presentation, and the contestants will race to solve the problem (with pencil and paper). 
 
@@ -39,8 +39,8 @@ export default function Rounds() {
         </div>
 
         <div className='bg-slate-900 p-4 rounded-md border-2 border-sky-900 xl:max-w-80'>
-            <h1 className={`${jbm.className} text-lg text-sky-300 mb-4`}>Estimathon</h1>
-            <div>
+            <h1 className={`${jbm.className} text-lg text-sky-300 mb-4 font-bold`}>Estimathon</h1>
+            <div className='text-slate-200'>
                 <p>
                 The Estimathon is a more fun and casual round involving the estimation of various interesting quantities. Teams have 30 minutes to work on a set of 13 estimation problems. Answers will be in interval form, and scores will be based on whether or not the interval contains the value, as well as the precision of the submitted interval.
                 </p>
