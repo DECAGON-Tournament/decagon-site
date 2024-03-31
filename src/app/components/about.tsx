4e7c7ced73c2e06c085c316c51dac5a51a6b7cb2
Ramy Kaddouri <106203063+rk234@users.noreply.github.com>
2024-03-31 13:55:41 -0400

    updates

--- src/app/components/about.tsx
+++ src/app/components/about.tsx
@@ -1,6 +1,6 @@
 import { JetBrains_Mono } from 'next/font/google'
 
-const jbm = JetBrains_Mono({subsets: ["latin"], style: ["normal"]})
+const jbm = JetBrains_Mono({ subsets: ["latin"], style: ["normal"] })
 
 export default function About() {
     return <div className='flex flex-col md:flex-row bg-slate-950 p-4 pt-16 md:p-8 md:pt-20 gap-9 md:gap-9 items-top'>
@@ -11,7 +11,7 @@ export default function About() {
         <div className='w-full md:w-2/3 flex flex-col gap-1'>
             <h1 className={`font-mono text-sky-300 font-bold text-2xl`}>What is DECAGON?</h1>
             <p>
-                The Centennial National Math Honor Society is partnering this year with the Glenelg National Math Honor Society to host the new DECAGON Tournament, a continuation of the annual CHS Mu Alpha Theta Tournament!
+                The Centennial National Math Honor Society is partnering this year with the Glenelg National Math Honor Society to host the new DECAGON Tournament, a continuation of the annual CHS Mu Alpha Theta Tournament! <span className="font-bold"> All levels of math competition experience are welcome. </span>
             </p>
             <p>
                 The tournament is a great way for competitors to test their math skills in a fun, low-stress competition.
@@ -21,19 +21,19 @@ export default function About() {
             </p>
             <h1 className={`font-mono text-sky-300 font-bold text-2xl mt-4`}>Location</h1>
             <p>
-                This year, we will be hosting at two locations: Centennial High School 
+                This year, we will be hosting at two locations: Centennial High School
                 <a className="link mx-1 text-sky-50" href="https://maps.app.goo.gl/vRjrU1QYevxyWkRS8" target="_blank" rel="noopener noreferrer">(4300 Centennial Lane, Ellicott City, MD, 21042)</a>
-                and Glenelg High School 
+                and Glenelg High School
                 <a className="link mx-1 text-sky-50" href="https://maps.app.goo.gl/hD43XwYAafoT5kobA" target="_blank" rel="noopener noreferrer">(14025 Burntwoods Rd, Glenelg, MD 21737)</a>
 
             </p>
             <h1 className={`font-mono text-sky-300 font-bold text-2xl mt-4`}>Lunch</h1>
             <p>
-                <span className='font-bold'>Lunch is free!</span> We will be serving cheese and pepperoni pizza.
+                <span className='font-bold'>Lunch is free at Glenelg!</span> Lunch can be purchased for cash at Centennial. We will be serving cheese and pepperoni pizza.
             </p>
             <h1 className={`font-mono text-sky-300 font-bold text-2xl mt-4`}>Test Guidelines</h1>
             <p>
-            All answers should be in simplest form. The only permitted materials are pencils and paper. Computational aids, including but not limited to calculators, rulers, protractors, and graph paper are strictly prohibited. 
+                All answers should be in simplest form. The only permitted materials are pencils and paper. Computational aids, including but not limited to calculators, rulers, protractors, and graph paper are strictly prohibited.
             </p>
         </div>
     </div>
