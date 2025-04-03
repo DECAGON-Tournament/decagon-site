220ad53c17a718a2efd3629011591f30fae20b72
Jaden Li <jadenmengl@gmail.com>
2025-04-03 10:39:54 -0400

    added mathworks

--- src/app/components/sponsors.tsx
+++ src/app/components/sponsors.tsx
@@ -31,6 +31,9 @@ export default function Sponsors() {
           <a href="https://www.fiverings.com/" className="bg-slate-500 rounded-lg p-4" target='_blank'>
             <img src="sponsors/fiverings.png" className='h-24' alt="Five Rings Logo" />
           </a>
+          <a href="https://www.mathworks.com/" className="bg-slate-500 rounded-lg p-4" target='_blank'>
+            <img src="sponsors/mathworks.png" className='h-24' alt="Mathworks Logo" />
+          </a>
           <p className="text-lg">
             Interested in sponsoring us? Take a look at our <a className="link text-sky-50" href="prospectus.pdf" download="decagon_prospectus" target="_blank" rel="noopener noreferrer">prospectus</a> and email us at <a className="link text-sky-50" href="mailto:decagontournament@gmail.com" target="_blank" rel="noopener noreferrer">decagontournament@gmail.com</a>!
           </p>
