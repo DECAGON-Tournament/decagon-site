dc585aeb22da3914ec2f2c777da7b980a366a8ea
JoeLi1234 <joejoezl2008@gmail.com>
2026-02-26 15:54:02 -0500

    Update schedule.tsx

--- src/app/components/schedule.tsx
+++ src/app/components/schedule.tsx
@@ -22,28 +22,28 @@ export default function Schedule() {
                         <td>Check-in</td>
                     </tr>
                     <tr>
-                        <td>9:00 AM - 9:20 AM</td>
+                        <td>9:00 AM - 9:45 AM</td>
                         <td>Opening Ceremony</td>
                     </tr>
                     <tr>
-                        <td>9:25 AM - 10:20 AM</td>
+                        <td>9:50 AM - 10:30 AM</td>
                         <td>Individual Round 1</td>
                     </tr>
                     <tr>
-                        <td>10:25 AM - 11:20 AM</td>
+                        <td>10:35 AM - 11:15 AM</td>
                         <td>Individual Round 2</td>
                     </tr>
                     <tr>
-                        <td>11:25 AM - 12:00 PM</td>
+                        <td>11:20 AM - 11:55 PM</td>
                         <td>Team Round</td>
                     </tr>
                     <tr>
-                        <td>12:05 PM - 12:50 PM</td>
+                        <td>12:00 PM - 12:45 PM</td>
                         <td>Lunch</td>
                     </tr>
                     <tr>
-                        <td>12:55 PM - 1:30 PM</td>
-                        <td>Theme Round</td>
+                        <td>12:50 PM - 1:30 PM</td>
+                        <td>Guts Round</td>
                     </tr>
                     <tr>
                         <td>1:35 PM - 2:10 PM</td>
