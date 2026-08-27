88f942ee6f6ff5b1a268bbe2ee636a7730982c74
MaybeStev <107894786+MaybeStev@users.noreply.github.com>
2026-08-26 21:12:32 -0400

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,6 +7,7 @@ import Schedule from './components/schedule'
 import Sponsors from './components/sponsors'
 import Photos from './components/photos'
 
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col bg-black">
